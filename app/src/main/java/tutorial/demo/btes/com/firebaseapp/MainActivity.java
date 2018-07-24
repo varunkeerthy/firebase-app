df05021e6e967d58c6735cdcbbdff31f721c4f13
Varun Keerthy <vkeerthy@btes.com.au>
2018-07-24 13:20:48 +1000

    Feature: Fixed gradle dependencies for Firebase Database and Authentication module.

--- app/src/main/java/tutorial/demo/btes/com/firebaseapp/MainActivity.java
+++ app/src/main/java/tutorial/demo/btes/com/firebaseapp/MainActivity.java
@@ -1,38 +1,56 @@
 package tutorial.demo.btes.com.firebaseapp;
 
+import android.opengl.ETC1;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    FirebaseDatabase database;
-    DatabaseReference myRef;
+    private  FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private EditText userEmail;
+    private EditText userPassword;
+
+    private EditText emailText;
+    private EditText passText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        database = FirebaseDatabase.getInstance();
-    }
+        emailText = (EditText) findViewById(R.id.userEmail);
+        passText = (EditText) findViewById(R.id.userPassword);
 
-    protected void buttonClicked(View view) {
 
-        EditText name = (EditText) findViewById(R.id.name);
-        EditText contact = (EditText) findViewById(R.id.contact);
-        String childRecordName = name.getText().toString();
-        myRef = database.getReference("users").child(childRecordName);
+        mAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
-        myRef.child("name").setValue(childRecordName);
-        myRef.child("contact").setValue(contact.getText().toString());
+            }
+        };
+
 
 
     }
